d388308a7ee347e0a0947c135235a262a5306c4f
niqitosiq <nikitashtanko@yahoo.com>
2020-09-30 11:30:12 +0400

    task-1_docker init

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier',
+    'prettier/vue',
+  ],
+  rules: {
+    'vue/name-property-casing': ['error', 'kebab-case'],
+    'vue/no-v-html': 0,
+    indent: ['error', 2],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
