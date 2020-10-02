e96abf18b332ed6de32fcfe639e722eae6587181
niqitosiq <nikitashtanko@yahoo.com>
2020-10-02 19:13:45 +0400

    task-1_docker fix

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,6 +2,11 @@ export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
 
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'frontend',
