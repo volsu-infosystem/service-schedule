a76851979a98528aec8dd4c6b855cca68bd53788
DanielTenebris <danieltenebris@gmail.com>
2020-10-27 14:55:00 +0000

    :arrow_up: Add Schedule Module to Imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileModule } from './profile/profile.module';
 import { UserModule } from './user/user.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ProfileModule, UserModule],
+  imports: [TypeOrmModule.forRoot(), ProfileModule, UserModule, ScheduleModule],
   controllers: [],
   providers: [],
 })
