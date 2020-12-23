f929d878b52026bd03e3e3eba334148a015ebb59
DanielTenebris <danieltenebris@gmail.com>
2020-12-23 09:18:11 +0000

    :recycle: #12 Minor fix in auth

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -46,7 +46,6 @@ export class AuthService {
 
     const res = {
       status: 200,
-      user: currUser,
     };
     return res;
   }
--- backend/src/auth/interfaces/registerResponse.interface.ts
+++ backend/src/auth/interfaces/registerResponse.interface.ts
@@ -1,6 +1,3 @@
-import { UserEntity } from 'src/user/entities/user.entity';
-
 export interface RegisterResponse {
   readonly status: number;
-  readonly user?: UserEntity;
 }
