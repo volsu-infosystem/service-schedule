b47b8cdd2da851cc864684bb84d515f9ecda0cae
niqitosiq <shtanko@insigne.ru>
2021-03-01 13:25:17 +0300

    #27 Удаление старого фронта

--- frontend/old/api/api.js
+++
@@ -1,9 +0,0 @@
-import { Institute } from './apis/Institute';
-import { Auth } from './apis/Auth';
-
-export class Api {
-  constructor(http) {
-    this.institute = new Institute(http, 'institute');
-    this.auth = new Auth(http, 'auth');
-  }
-}
--- frontend/old/middleware/auth.js
+++
@@ -1,5 +0,0 @@
-export default function ({ store, redirect }) {
-  if (!store.state.isAuthenticated) {
-    return redirect('/login');
-  }
-}
