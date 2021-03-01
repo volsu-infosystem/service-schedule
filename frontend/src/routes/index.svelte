d8fa15e6167dabf099b370c3f8a943fd1ea54d87
niqitosiq <shtanko@insigne.ru>
2021-03-01 12:23:06 +0300

    #27 Замена лишнего

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,27 +1,7 @@
 <script>
-  import Main from '../components/blocks/main/Main.svelte';
-  import Service from '../components/blocks/service/Service.svelte';
-  import Services from '../components/blocks/services/Services.svelte';
-  import Advantages from '../components/blocks/advantages/Advantages.svelte';
-  import QuizSection from '../components/blocks/quiz/QuizSection.svelte';
-  import Contacts from '../components/blocks/contacts/Contacts.svelte';
-
-  import { initAnimations } from '../components/utils/parallax';
-
-  import { onMount } from 'svelte';
-
-  onMount(() => {
-    initAnimations();
-  });
 </script>
 
 <div class="index">
-  <Main />
-  <Service />
-  <Services />
-  <Advantages />
-  <QuizSection />
-  <Contacts />
 </div>
 
 <style lang="scss">
