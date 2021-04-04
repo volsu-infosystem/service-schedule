7614eda0365937717d909255704242048c09940f
niqitosiq <nikitashtanko@yahoo.com>
2021-04-04 21:27:03 +0300

    #49 Вывод

--- frontend/src/components/edit/ScheduleRow.svelte
+++ frontend/src/components/edit/ScheduleRow.svelte
@@ -99,7 +99,7 @@
     width: 100%;
     height: 100%;
     display: flex;
-    flex-direction: column;
     align-items: stretch;
+    position: relative;
   }
 </style>
--- frontend/src/components/edit/ScheduleSubCell.svelte
+++ frontend/src/components/edit/ScheduleSubCell.svelte
@@ -1,8 +1,17 @@
 <script>
   export let subcell
+
+  let marginTop
+  let marginBottom
+  $: {
+    const firstLesson = subcell.lessons[0]
+    marginTop = firstLesson && firstLesson.periodicity === 'den'
+    const lastLesson = subcell.lessons[subcell.lessons.length - 1]
+    marginBottom = lastLesson && lastLesson.periodicity === 'num'
+  }
 </script>
 
-<div class="subcell">
+<div class="subcell" class:marginTop class:marginBottom>
   {#each subcell.lessons as lesson}
     <div class="lesson">
       <span class="discipline">
@@ -23,20 +32,50 @@
 <style lang="scss">
   .subcell {
     display: flex;
+    flex-direction: column;
     width: 100%;
     height: 100%;
+    min-width: 0%;
     min-height: 100px;
-    background-color: var(--extra-light);
     position: relative;
-    &:nth-child(2n) {
-      border-left: solid 1px var(--gray);
+    border-left: solid 1px rgb(235, 235, 235);
+    &:first-child {
+      border-left: none;
+    }
+    &.marginBottom {
+      &:after {
+        content: '';
+        min-height: 80px;
+        height: 50%;
+        width: 100%;
+      }
+      .lesson {
+        border-bottom: solid 1px var(--gray);
+      }
+    }
+    &.marginTop {
+      &:before {
+        content: '';
+        min-height: 80px;
+        height: 50%;
+        width: 100%;
+      }
+      .lesson {
+        border-top: solid 1px var(--gray);
+      }
     }
   }
   .lesson {
     padding: 12px 9px;
     display: flex;
     flex-direction: column;
+    background-color: var(--extra-light);
     width: 100%;
+    height: 100%;
+    min-height: 80px;
+    &:nth-child(2n) {
+      border-top: solid 1px var(--gray);
+    }
   }
   .discipline {
     font-size: 14px;
